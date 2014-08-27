7a5c989a4628061919660babc0c09db66bef94d0
ganaraj <ganarajpr@gmail.com>
2014-08-27 22:15:31 +0100

    adding call site.

--- main.js
+++ main.js
@@ -2,35 +2,47 @@ var esprima = require('esprima');
 var fs = require('vinyl-fs');
 var map = require('map-stream');
 
+var callsite = require('./lib/callsite');
+
 var files = [];
+var esprimaOptions = {
+    tolerant : true
 
-function withEachFile(file){
+};
 
+function withEachFile(file,next){
     storeFile(file);
-
+    next(null,file);
 }
 
 
 function storeFile(file){
     if(file._contents){
         var contentString = file._contents.toString();
-        files.push({
-            path : file.path,
-            content : contentString,
-            ast : esprima.parse(contentString)
-        });
+        try{
+            files.push({
+                path : file.path,
+                content : contentString,
+                ast : esprima.parse(contentString,esprimaOptions)
+            });
+        }
+        catch(c){
+            console.warn(file.path);
+        };
+
     }
 }
 
 function onAllRead(){
-
+    callsite.build(files);
 }
 
 
 module.exports.process = function(glob){
-    fs.src(glob)
-        .pipe(map(withEachFile))
-        .pipe(onAllRead);
+    var r = fs.src(glob);
+        r.pipe(map(withEachFile));
+    r.on('end',onAllRead);
+
 };
 
 
