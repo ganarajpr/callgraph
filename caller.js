f8ddad5e6df230fdd9467252a0f4413f98c827a2
ganaraj <ganarajpr@gmail.com>
2014-08-28 21:23:14 +0100

    Finding function declarations.

--- caller.js
+++ caller.js
@@ -1,7 +1,19 @@
 var callgraph = require('./main');
+var resolve = require("resolve");
 
 
 var dir = '../Ngprima/codequery/*.js';
 
 
+/*var opts = {
+    basedir : dir
+};
+
+
+resolve("./codequery/cq",opts,function(err,res){
+    console.log(res);
+});*/
+
+
 callgraph.process(dir);
+
